093aeeef5bf02a1194e281ae6f5e326ff2da117f
Tomeshwar Shiwane <Tomeshwar Shiwane@IB-Pune-LAP-052>
2020-05-02 01:13:02 +0530

    initial checkin

---
+++ src/Information.java
@@ -0,0 +1,12 @@
+
+public class Information {
+	
+// Step 1 : create a empty git repo.
+	//  create a empty project in sts.  
+	// Right click >> share project >> 
+	// create repo.
+	// Add code commit (Give hhtp Url >> Commit . 
+	// Done 
+	
+
+}
